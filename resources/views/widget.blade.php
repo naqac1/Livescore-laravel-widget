be8a06c06645c5367b6b4829fccc6d7b88f081f7
naqac1 <electronique1@gmail.com>
2024-06-21 22:09:54 -0700

    New Commit

---
+++ resources/views/widget.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Match live score</title>
+</head>
+<body>
+<div class="match-all">
+    <div class="match-container">
+        <div class="match-top">
+            <div class="match-status">Live</div>
+            <div class="match-league"><img src="https://upload.wikimedia.org/wikipedia/commons/0/0f/LaLiga_logo_2023.svg" alt="">La liga</div>
+        </div>
+
+    <div class="match-content">
+        <div class="match-column">
+            <div class="team home">
+                <div class="logo">
+                    <img src="https://upload.wikimedia.org/wikipedia/fr/a/a1/Logo_FC_Barcelona.svg" alt="">
+                </div>
+                <h2 class="team-name">Barça</h2>
+            </div>
+        </div>
+        <div class="match-column">
+            <div class="match-details">
+                <div class="match-time">
+                    13
+                </div>
+                <div class="match-score">
+                    <span class="match-score-number">1</span>
+                    <span class="match-score-separation">:</span>
+                    <span class="match-score-number">1</span>
+                </div>
+                <div class="match-time-lapsed">
+                    72'
+                </div>
+                <div class="match-referee">
+                    <h2>Mike jean</h2>
+                </div>
+            </div>
+        </div>
+        <div class="match-column">
+            <div class="team away">
+                <div class="logo">
+                    <img src="https://upload.wikimedia.org/wikipedia/fr/a/a1/Logo_FC_Barcelona.svg" alt="">
+                </div>
+                <h2 class="team-name">Real Madrid</h2>
+            </div>
+        </div>
+    </div>
+    </div>
+</div>
+</body>
+</html>
